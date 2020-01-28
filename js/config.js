96afdcfdf074d4de7256a8ec474687c96975db28
Gavin He <hscgavin@gmail.com>
2020-01-28 23:51:00 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_stue9e4OT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9pcgqsqjq9de9r9e2kc0ge6bi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
